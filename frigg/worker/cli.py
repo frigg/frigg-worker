616e656cb9390321cb36d8f1b067d0bddaff11c2
Rolf Erik Lekang <me@rolflekang.com>
2014-10-21 10:20:05 +0200

    Add cli argument parser

---
+++ frigg/worker/cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+from fabric import colors
+from frigg.worker.fetcher import fetcher
+
+
+class Commands(object):
+
+    @staticmethod
+    def start():
+        print(colors.green("Starting frigg worker"))
+        fetcher()
+
+    @staticmethod
+    def unknown_command():
+        print(colors.red("Unknown command"))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Do some work for frigg.')
+    parser.add_argument('command')
+
+    args = parser.parse_args()
+
+    getattr(Commands, args.command, Commands.unknown_command)()
