908df99924893199f4405489802e76dd8947ac84
Rolf Erik Lekang <me@rolflekang.com>
2014-11-05 10:11:44 +0100

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frigg-worker',
-    version='0.0.1',
+    version='0.1.0',
     description='',
     author='The frigg team',
     author_email='hi@frigg.io',
