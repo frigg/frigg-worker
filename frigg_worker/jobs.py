df07d82d4fbcc1b8a106677d5291a4f1824a4c74
Fredrik Carlsen <fredrik@carlsen.io>
2015-04-12 16:28:57 +0200

    Adds start_services

--- frigg_worker/jobs.py
+++ frigg_worker/jobs.py
@@ -84,7 +84,13 @@ class Build(object):
     def settings(self):
         return build_settings(self.working_directory, self.docker)
 
+    def start_services(self):
+        for service in self.settings['services']:
+            self.docker.run("sudo service {0} start".format(service))
+
     def run_tests(self):
+        self.start_services()
+
         task = None
         self.delete_working_dir()
         if not self.clone_repo():
