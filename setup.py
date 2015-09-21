7d359761ff70a669d766d77707b73ae3a70f57e2
Rolf Erik Lekang <me@rolflekang.com>
2015-09-21 06:21:04 +0200

    fix(deps): Set min bound of frigg-settings to 1.1.1
    The release contains a fix that this project needs.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'click==5.1',
         'frigg-coverage>=1.0.0,<2.0.0',
-        'frigg-settings>=1.0.0,<2.0.0',
+        'frigg-settings>=1.1.1,<2.0.0',
         'docker-wrapper>=2.0,<2.1',  # rq.filter: <2.0
         'pyyaml==3.11',
         'requests>=2.0.0,<3.0.0',
