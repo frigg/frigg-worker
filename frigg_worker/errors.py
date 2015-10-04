0fad3c6878a7a43793b9b0b15228563b034e0b96
Rolf Erik Lekang <me@rolflekang.com>
2015-10-04 13:57:40 +0200

    chore(errors): Add GitCloneError
    This will be used to enhance the handling of git clone errors in
    frigg-worker.

--- frigg_worker/errors.py
+++ frigg_worker/errors.py
@@ -1,8 +1,32 @@
+import socket
+
+
 class FriggWorkerBaseError(Exception):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.host = socket.getfqdn()
 
 
 class ApiError(FriggWorkerBaseError):
     def __init__(self, error):
         self.code = error['code']
         super().__init__(error['message'])
+
+
+class GitCloneError(FriggWorkerBaseError):
+    MESSAGES = {
+        'MISSING_BRANCH': 'Branch does not exist',
+        'MISSING_COMMIT': 'Commit does not exist',
+        'DNS': 'Can not resolve host',
+        'EMPTY_REPLY': 'Empty reply from vcs server',
+        'UNKNOWN': 'Unknown error cloning',
+    }
+
+    def __init__(self, type, stdout, stderr, report_build_as_error, worker_should_quit=False):
+        self.type = type
+        self.stdout = stdout
+        self.stderr = stderr
+        self.report_build_as_error = report_build_as_error
+        self.worker_should_quit = worker_should_quit
+        super().__init__(self.MESSAGES[type])
