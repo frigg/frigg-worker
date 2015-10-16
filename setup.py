956da89dd1b32b1399b937ec123cd3f12f941daf
Depsy <contact@depsy.io>
2015-10-16 22:39:19 +0000

    Upgrade dependency requests to >=2.8.1,<3.0.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'frigg-settings>=1.1.1,<2.0.0',
         'docker-wrapper>=2.1,<2.2',  # rq.filter: <2.0
         'pyyaml==3.11',
-        'requests>=2.0.0,<3.0.0',
+        'requests>=2.8.1,<3.0.0',
         'raven==5.7.2'
     ],
     entry_points={
